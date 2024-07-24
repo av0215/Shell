aa7ee10bed18f01c58e8a1566dff90c60cb54453
av0215 <152055641+av0215@users.noreply.github.com>
2024-07-24 13:26:03 +0530

    Add files via upload
    Shell script for jenkins and mvn

---
+++ jenkinsmvn.sh
@@ -0,0 +1,14 @@
+apt-get update 
+
+apt install OpenJDK-17-jre 
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install Jenkins -y
+
+sudo apt install mvn -y 
+
